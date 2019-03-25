556e5e660d4469a0da9c5e2d56e4c8c1681bae0d
fisker <lionkay@gmail.com>
2019-03-25 16:14:50 +0800

    style: fix code style

--- configs/prettier/prettier.js
+++ configs/prettier/prettier.js
@@ -1,8 +1,6 @@
 module.exports = {
   plugins: ['prettier'],
-  extends: ['eslint-config-prettier'].map(
-    require.resolve
-  ),
+  extends: ['eslint-config-prettier'].map(require.resolve),
   rules: {
     // specify curly brace conventions for all control statements
     curly: ['error', 'all'],
