721b548ab6cee3c5c7200301393b6660b5c4fa68
fisker Cheung <lionkay@gmail.com>
2021-05-06 17:25:25 +0800

    Update parser options

--- scripts/rules.mjs
+++ scripts/rules.mjs
@@ -1,6 +1,6 @@
 import {join} from 'path';
 import {readFileSync} from 'fs';
-import createEsmUtils from 'esm-utils'
+import createEsmUtils from 'esm-utils';
 import printer from './shared/markdown-rules-printer.mjs';
 import writeFile from './shared/write-file.mjs';
 import getRules from './shared/get-rules.mjs';
@@ -8,7 +8,7 @@ import parseRuleId from './shared/parse-rule-id.mjs';
 import sortRuleId from './shared/sort-rule-id.mjs';
 import config from '../index.js';
 
-const {dirname} = createEsmUtils(import.meta)
+const {dirname} = createEsmUtils(import.meta);
 
 const rules = getRules(config);
 const data = Object.keys(rules)
